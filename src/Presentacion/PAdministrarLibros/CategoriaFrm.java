c07a0aa35fbb749eb3504d02f6c8b19589bb08d5
Mauricio Ballesteros <mballes95@gmail.com>
2017-05-01 22:16:30 -0400

    CRUD Categoria Terminado

--- src/Presentacion/PAdministrarLibros/CategoriaFrm.java
+++ src/Presentacion/PAdministrarLibros/CategoriaFrm.java
@@ -5,6 +5,10 @@
  */
 package Presentacion.PAdministrarLibros;
 
+import Negocio.NAdministrarLibros.CategoriaNegocio;
+import java.util.Arrays;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author mauriballes
@@ -14,10 +18,56 @@ public class CategoriaFrm extends javax.swing.JFrame {
     /**
      * Creates new form CategoriaFrm
      */
+    private CategoriaNegocio m_CategoriaNegocio;
+
     public CategoriaFrm() {
         initComponents();
         this.setTitle("Gestionar Categoria");
         this.setLocationRelativeTo(null);
+        m_CategoriaNegocio = new CategoriaNegocio();
+        inicializar();
+    }
+
+    public void eliminarCategoria() {
+        int fila = tableCategoria.getSelectedRow();
+        DefaultTableModel categoriasUpdated = (DefaultTableModel) tableCategoria.getModel();
+        m_CategoriaNegocio.eliminarCategoria(Integer.parseInt(categoriasUpdated.getValueAt(fila, 0).toString()));
+        categoriasUpdated.removeRow(fila);
+        textNombre.setText("");
+        textDescripcion.setText("");
+    }
+
+    public void inicializar() {
+        obtenerCategoria();
+    }
+
+    public void modificarCategoria() {
+        String[] tableHeader = new String[]{"id", "nombre", "descripcion"};
+        int fila = tableCategoria.getSelectedRow();
+        DefaultTableModel categoriasUpdated = (DefaultTableModel) tableCategoria.getModel();
+        m_CategoriaNegocio.modificarCategoria(
+                Integer.parseInt(categoriasUpdated.getValueAt(fila, Arrays.asList(tableHeader).indexOf("id")).toString()),
+                textNombre.getText(),
+                textDescripcion.getText());
+        categoriasUpdated.setValueAt(textNombre.getText(), fila, Arrays.asList(tableHeader).indexOf("nombre"));
+        categoriasUpdated.setValueAt(textDescripcion.getText(), fila, Arrays.asList(tableHeader).indexOf("descripcion"));
+    }
+
+    public void obtenerCategoria() {
+        DefaultTableModel categorias = m_CategoriaNegocio.obtenerCategorias();
+        tableCategoria.setModel(categorias);
+    }
+
+    public void registrarCategoria() {
+        int id = m_CategoriaNegocio.registrarCategoria(
+                textNombre.getText(),
+                textDescripcion.getText());
+        DefaultTableModel categoriasUpdated = (DefaultTableModel) tableCategoria.getModel();
+        categoriasUpdated.addRow(new Object[]{
+            id,
+            textNombre.getText(),
+            textDescripcion.getText()
+        });
     }
 
     /**
@@ -33,7 +83,7 @@ public class CategoriaFrm extends javax.swing.JFrame {
         buttonModificar = new javax.swing.JButton();
         buttonEliminar = new javax.swing.JButton();
         textDescripcion = new javax.swing.JTextField();
-        textDireccion = new javax.swing.JTextField();
+        textNombre = new javax.swing.JTextField();
         labelNombre = new javax.swing.JLabel();
         labelDescripcion = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -42,10 +92,25 @@ public class CategoriaFrm extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         buttonRegistrar.setText("Registrar");
+        buttonRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonRegistrarActionPerformed(evt);
+            }
+        });
 
         buttonModificar.setText("Modificar");
+        buttonModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonModificarActionPerformed(evt);
+            }
+        });
 
         buttonEliminar.setText("Eliminar");
+        buttonEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonEliminarActionPerformed(evt);
+            }
+        });
 
         labelNombre.setText("Nombre");
 
@@ -62,6 +127,11 @@ public class CategoriaFrm extends javax.swing.JFrame {
                 "Id", "Nombre", "Descripcion"
             }
         ));
+        tableCategoria.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableCategoriaMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tableCategoria);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -89,7 +159,7 @@ public class CategoriaFrm extends javax.swing.JFrame {
                         .addComponent(labelDescripcion)
                         .addGap(22, 22, 22)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(textDireccion)
+                    .addComponent(textNombre)
                     .addComponent(textDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -99,7 +169,7 @@ public class CategoriaFrm extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(labelNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(textDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(textNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(labelDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -116,6 +186,26 @@ public class CategoriaFrm extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonRegistrarActionPerformed
+        registrarCategoria();
+    }//GEN-LAST:event_buttonRegistrarActionPerformed
+
+    private void tableCategoriaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableCategoriaMouseClicked
+        int fila = tableCategoria.getSelectedRow();
+        String[] tableHeader = new String[]{"id", "nombre", "descripcion"};
+        DefaultTableModel categorias = (DefaultTableModel) tableCategoria.getModel();
+        textNombre.setText(String.valueOf(categorias.getValueAt(fila, Arrays.asList(tableHeader).indexOf("nombre"))));
+        textDescripcion.setText(String.valueOf(categorias.getValueAt(fila, Arrays.asList(tableHeader).indexOf("descripcion"))));
+    }//GEN-LAST:event_tableCategoriaMouseClicked
+
+    private void buttonModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonModificarActionPerformed
+        modificarCategoria();
+    }//GEN-LAST:event_buttonModificarActionPerformed
+
+    private void buttonEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonEliminarActionPerformed
+        eliminarCategoria();
+    }//GEN-LAST:event_buttonEliminarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -161,6 +251,6 @@ public class CategoriaFrm extends javax.swing.JFrame {
     private javax.swing.JLabel labelNombre;
     private javax.swing.JTable tableCategoria;
     private javax.swing.JTextField textDescripcion;
-    private javax.swing.JTextField textDireccion;
+    private javax.swing.JTextField textNombre;
     // End of variables declaration//GEN-END:variables
 }
